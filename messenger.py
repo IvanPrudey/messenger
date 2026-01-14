9dc90a0b7e1c673ce2eaaf6fba9dee64c7628348
IvanPrudey <prudey.ia@yandex.ru>
2026-01-14 22:30:24 +0300

    main()

--- messenger.py
+++ messenger.py
@@ -15,3 +15,11 @@ class SimpleMessenger:
 
     def clear_screen(self):
         os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
