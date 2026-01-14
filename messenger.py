dc61d5ffb02731d22ce2351d7fd26cf280eb67a6
IvanPrudey <prudey.ia@yandex.ru>
2026-01-14 22:28:09 +0300

    add_SimpleMessenger

--- messenger.py
+++ messenger.py
@@ -3,3 +3,15 @@ import socket
 import sys
 import time
 import threading
+
+
+class SimpleMessenger:
+
+    def __init__(self):
+        self.running = True
+        self.host = '127.0.0.1'
+        self.port = 5555
+        self.nickname = input('введите Ваш никнейм : ')
+
+    def clear_screen(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
